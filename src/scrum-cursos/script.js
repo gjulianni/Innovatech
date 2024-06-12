1d8d4e799935a1a344547aed6ef41025b632733e
valcir-jr <162353778+valcir-jr@users.noreply.github.com>
2024-06-12 19:36:37 -0300

    Delete src directory

--- src/scrum-cursos/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    document.getElementById('menu').classList.toggle('active');
-});
-
-document.getElementById('closeButton').addEventListener('click', function() {
-    document.getElementById('menu').classList.remove('active');
-});
